56ba3aee7ed6fcd952392c3d5be213ce59c5487e
JoakimJohnsson <joakim.johnsson@su.se>
2020-02-12 12:25:17 +0100

    add sass files - testing sass structure

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,26 @@
-import React from 'react';
-const App = () => (
-    <div>
-        <h1>App</h1>
-    </div>
-);
+// import React from 'react';
+// const App = () => (
+//     <div>
+//         <h1>App</h1>
+//     </div>
+// );
+// export default App;
+
+// Test code for SASS files
+import React, { Component } from 'react';
+import Navigation from '../Navigation';
+const LINKS = [
+    { label: 'Website', to: 'https://www.robinwieruch.de/' },
+    { label: 'Twitter', to: 'https://twitter.com/rwieruch' },
+];
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Navigation links={LINKS} />
+            </div>
+        );
+    }
+}
 export default App;
+// end SASS test code
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,7 +1,24 @@
+// import React from 'react';
+// const Navigation = () => (
+//     <div>
+//         <h1>Navigation</h1>
+//     </div>
+// );
+// export default Navigation;
+
+// Test code for SASS files
 import React from 'react';
-const Navigation = () => (
-    <div>
-        <h1>Navigation</h1>
+import '../../css/components/_navigation.scss';
+const Navigation = ({ links }) => (
+    <div className="Navigation">
+        <ul>
+            {links.map(link => (
+                <li key={link.to}>
+                    <a href={link.to}>{link.label}</a>
+                </li>
+            ))}
+        </ul>
     </div>
 );
 export default Navigation;
+// end SASS test code

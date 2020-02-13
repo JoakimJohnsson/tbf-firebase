5d6b2df956c652f0c9601224321c10444d5d9c77
JoakimJohnsson <joakim.johnsson@su.se>
2020-02-13 19:10:20 +0100

    initial commit for responsive navbar

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,49 +1,49 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
 import SignOutButton from "../SignOut";
 import '../../css/components/_navigation.scss';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
 import AuthUserContext from "../Session/context";
 import TbfLogo from "../Logo";
 import {TbfLogoText} from "../Logo";
 
 const Navigation = () => (
-    <div className="navigation">
-        <TbfLogo/>
-        <TbfLogoText/>
-        <AuthUserContext.Consumer>
-            {authUser => authUser ? <NavigationAuth/> : <NavigationNonAuth/>}
-        </AuthUserContext.Consumer>
-    </div>
+
+    <Navbar className="tbf-main-nav" expand="lg">
+        <Navbar.Brand href="#home">
+            <TbfLogo/>
+            <TbfLogoText/>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Collapse id="basic-navbar-nav">
+            <AuthUserContext.Consumer>
+                {authUser => authUser ? <NavigationAuth/> : <NavigationNonAuth/>}
+            </AuthUserContext.Consumer>
+            <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2"/>
+                <Button variant="outline-success">Search</Button>
+            </Form>
+        </Navbar.Collapse>
+    </Navbar>
 );
 const NavigationAuth = () => (
-    <ul>
-        <li>
-            <Link to={ROUTES.LANDING}>Landing</Link>
-        </li>
-        <li>
-            <Link to={ROUTES.HOME}>Home</Link>
-        </li>
-        <li>
-            <Link to={ROUTES.ACCOUNT}>Account</Link>
-        </li>
-        <li>
-            <Link to={ROUTES.ADMIN}>Admin</Link>
-        </li>
-        <li>
-            <SignOutButton/>
-        </li>
-    </ul>
+    <Nav className="mr-auto">
+        <Nav.Link href={ROUTES.LANDING}>Landing</Nav.Link>
+        <Nav.Link href={ROUTES.HOME}>Home</Nav.Link>
+        <Nav.Link href={ROUTES.ACCOUNT}>Account</Nav.Link>
+        <Nav.Link href={ROUTES.ADMIN}>Admin</Nav.Link>
+        <SignOutButton/>
+    </Nav>
 );
 const NavigationNonAuth = () => (
-    <ul>
-        <li>
-            <Link to={ROUTES.SIGN_IN}>Sign in</Link>
-        </li>
-        <li>
-            <Link to={ROUTES.LANDING}>Landing</Link>
-        </li>
-    </ul>
+    <Nav className="mr-auto">
+        <Nav.Link href={ROUTES.SIGN_IN}>Sign in</Nav.Link>
+        <Nav.Link href={ROUTES.LANDING}>Landing</Nav.Link>
+    </Nav>
 );
 
 export default Navigation;

2e42d86ce6601c3ecf74980f064fc5ebe5228081
JoakimJohnsson <joakim.johnsson@su.se>
2020-02-16 13:05:39 +0100

    footer changes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,9 +3,9 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const Footer = () => (
     <footer className="container-fluid standard-container">
-        <div className="row">
-            <div className="col-12 small">
-                <p><FontAwesomeIcon icon={['far', 'copyright']} /> 2020 Joakim Johnsson</p>
+        <div className="row h-100">
+            <div className="col-12 small d-flex align-items-center justify-content-center">
+                <span><FontAwesomeIcon icon={['far', 'copyright']} /> 2020 Joakim Johnsson</span>
             </div>
         </div>
     </footer>

95d18fc4433471a3f89d84078fffe6eca37fbbb4
JoakimJohnsson <joakim.johnsson@su.se>
2020-06-11 21:39:41 +0200

    styles - working forms and lists

--- src/constants/nodes.js
+++ src/constants/nodes.js
@@ -1,19 +1,15 @@
 /* Node names as used in Firebase DB */
 export const PERSONS = 'persons';
-export const AFFILIATIONS = 'affiliations';
 export const ROLES = 'roles';
 export const SONGS = 'songs';
 export const ARTISTS = 'artists';
 export const IMAGES = 'images';
-export const ANNAT = 'annat';
 
 /* List with node names - used to dynamically create dashboard modals */
 export const NODES_LIST = [
     PERSONS,
-    AFFILIATIONS,
     ROLES,
     SONGS,
     ARTISTS,
-    IMAGES,
-    ANNAT
+    IMAGES
 ];

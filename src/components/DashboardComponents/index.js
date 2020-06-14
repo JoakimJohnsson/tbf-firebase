40731cbf079b9d9b0e4aef6015b419b94949da2f
JoakimJohnsson <joakim.johnsson@su.se>
2020-06-14 10:52:42 +0200

    compilation warning fix

--- src/components/DashboardComponents/index.js
+++ src/components/DashboardComponents/index.js
@@ -30,15 +30,15 @@ const DashboardCard = (props) => (
                 <nav aria-label="Page navigation example">
                     <ul className="pagination justify-content-center">
                         <li className="page-item disabled">
-                            <a className="page-link" href tabIndex="-1" aria-label="Previous">
+                            <a className="page-link" href="#/" tabIndex="-1" aria-label="Previous">
                                 <FontAwesomeIcon icon="chevron-left"/>
                             </a>
                         </li>
-                        <li className="page-item active"><a className="page-link" href>1</a></li>
-                        <li className="page-item"><a className="page-link" href>2</a></li>
-                        <li className="page-item"><a className="page-link" href>3</a></li>
+                        <li className="page-item active"><a className="page-link" href="#/">1</a></li>
+                        <li className="page-item"><a className="page-link" href="#/">2</a></li>
+                        <li className="page-item"><a className="page-link" href="#/">3</a></li>
                         <li className="page-item">
-                            <a className="page-link" href aria-label="Next">
+                            <a className="page-link" href="#/" aria-label="Next">
                                 <FontAwesomeIcon icon="chevron-right"/>
                             </a>
                         </li>

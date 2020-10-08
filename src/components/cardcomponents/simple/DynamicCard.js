49c92020bafea781c9bf069b22d715444d2dc5df
JoakimJohnsson <joakim.johnsson@su.se>
2020-10-08 09:41:30 +0200

    layout changes

--- src/components/cardcomponents/simple/DynamicCard.js
+++ src/components/cardcomponents/simple/DynamicCard.js
@@ -6,7 +6,7 @@ const DynamicCard = (props) => {
     let rand2 = Math.floor(Math.random() * 4) + 1;
 
     return (
-        <div className={`col-12 col-sm-6 col-xl-3 artist-card__simple p-3 logo-font-family__${rand.toString()}`} key={props.id}>
+        <div className={`col-6 col-sm-4 artist-card__simple p-3 logo-font-family__${rand.toString()}`} key={props.id}>
             <Link className={`text-color-variant__${rand2.toString()}`} to={`/${props.route}/${props.id}`}>
                 {props.name}
             </Link>

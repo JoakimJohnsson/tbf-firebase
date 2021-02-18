105d0dbb7b334f90ad8a4f450751ae6f9c823e97
JoakimJohnsson <joakim.johnsson@su.se>
2021-02-18 10:52:29 +0100

    functional song upload helper

--- src/components/Forms/SongUploadForm.js
+++ src/components/Forms/SongUploadForm.js
@@ -1,31 +1,129 @@
-import React from 'react';
+import React, {Component} from 'react';
 import firebase from "firebase";
 
-function doIt(e) {
-    e.preventDefault();
-    console.log('The link was clicked.');
-    // Add a new document in collection "cities"
-    firebase.firestore().collection("cities").doc("LA").set({
-        name: "Los Angeles",
-        state: "CA",
-        country: "USA"
-    })
-        .then(() => {
-            console.log("Document successfully written!");
+const INITIAL_STATE = {
+    songId: '',
+    artistId: '',
+    recordId: '',
+    index: 1,
+    numberOfPlays: 1,
+    error: null
+};
+
+class UploadSongForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ...INITIAL_STATE
+        };
+    }
+
+    onSubmit = event => {
+        firebase.firestore().collection('songs').doc(this.state.songId).set({
+            artistId: this.state.artistId,
+            recordId: this.state.recordId,
+            index: this.state.index,
+            numberOfPlays: this.state.numberOfPlays
         })
-        .catch((error) => {
-            console.error("Error writing document: ", error);
-        });
+            .then(() => {
+               this.setState({index: this.state.index +1});
+            })
+            .catch(error => {
+                this.setState({error})
+            });
+
+        event.preventDefault();
+    };
+
+    onChange = event => {
+        this.setState({[event.target.name]: event.target.value});
+    };
+
+    render() {
+        const {
+            songId,
+            artistId,
+            recordId,
+            index,
+            numberOfPlays,
+            error,
+        } = this.state;
+        const isInvalid = songId === '';
+        return (
+            <form onSubmit={this.onSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="songId" className="form-label">Song Id</label>
+                    <input
+                        name="songId"
+                        id="songId"
+                        value={songId}
+                        onChange={this.onChange}
+                        type="text"
+                        className="form-control"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="artistId" className="form-label">Artist Id</label>
+                    <select
+                        id="artistId"
+                        name="artistId"
+                        className="form-control"
+                        value={artistId}
+                        onChange={this.onChange}>
+                        <option value="zzz">zzz</option>
+                        <option value="the-baseball-field">The Baseball field</option>
+                    </select>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="recordId" className="form-label">Record Id</label>
+                    <select
+                        id="recordId"
+                        name="recordId"
+                        className="form-control"
+                        value={recordId}
+                        onChange={this.onChange}>
+                        <option value="zzz">zzz</option>
+                        <option value="record-lp">Record lp</option>
+                    </select>
+                </div>
+                <div className="mb-3 d-flex">
+                    <div className="mr-3">
+                        <label htmlFor="index" className="form-label">Index</label>
+                        <input
+                            name="index"
+                            id="index"
+                            value={index}
+                            onChange={this.onChange}
+                            type="number"
+                            min="1"
+                            max="35"
+                            className="form-control"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="numberOfPlays" className="form-label">Number of plays</label>
+                        <input
+                            name="numberOfPlays"
+                            id="numberOfPlays"
+                            value={numberOfPlays}
+                            onChange={this.onChange}
+                            type="number"
+                            min="1"
+                            max="1"
+                            className="form-control"
+                        />
+                    </div>
+                </div>
+
+                <div className="text-end">
+                    <button className="btn btn__neu btn-primary mb-3" disabled={isInvalid} type="submit">
+                        Submit
+                    </button>
+                </div>
+                {error && error.message}
+            </form>
+        );
+    }
 }
 
-const UploadSongForm = () => {
-    return (
-        <div>
-            <button className="btn btn-secondary"
-                    onClick={doIt}>
-                Do it
-            </button>
-        </div>
-    )
-}
 export default UploadSongForm;

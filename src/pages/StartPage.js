ae1edc60711960cfd48c16f2fbc3bae9cbd29c24
JoakimJohnsson <joakim.johnsson@su.se>
2021-02-23 11:57:04 +0100

    dynamic improvements

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -30,12 +30,16 @@ const StartPage = () => {
     return lastUpdatedArtistId.length && lastAddedRecordId.length && lastAddedRecordArtistId.length && countersAreFetched ?
         (<div className="row">
             <div className="standard-secondary-column">
-                <div className="standard-box-wrapper__near-dark">
+                <div className="standard-box-wrapper__near-dark mb-5">
                     <h1>The Baseball Field & Friends</h1>
                     <p>The music scene in Nyköping has delivered great music since the early 90's. </p>
                     <p>This website, former home of band <b>The Baseball Field</b> and music labels <b>Vanishing Vanity Music</b> and <b>Strandad
                         Sjöbuse Records</b>, now brings you that good old music in a modern way.</p>
-                    <p>Here is a <a href="/old">link</a> to the old site</p>
+                    <h2>Extra content</h2>
+                    <h3>Links</h3>
+                    <p><a href="/old">The old TBF/VVM site</a></p>
+                    <h3>Video</h3>
+                    <p>T.B.A</p>
                     <CopyrightInfoComponent className={"d-none d-lg-block"}/>
                 </div>
             </div>

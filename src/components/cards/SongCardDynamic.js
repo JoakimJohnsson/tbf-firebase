a73fdfc9a2bc24191f2855f8922a795aede22b21
JoakimJohnsson <joakim.johnsson@su.se>
2021-02-25 12:24:22 +0100

    fixed player refactoring

--- src/components/cards/SongCardDynamic.js
+++ src/components/cards/SongCardDynamic.js
@@ -18,13 +18,12 @@ const SongCardDynamic = ({id}) => {
 const SongCardDynamicContent = ({id}) => {
 
     const song = FetchSongFromId(id);
-    let rand = Math.floor(Math.random() * 8) + 1;
     return song.artistId ? (
             <>
-                <div className={`song-card-dynamic text-color-variant__${rand.toString()}`}>
-                    <div className="p-3 small"><span className="text-uppercase font-weight-bold"><Artist id={song.artistId}/></span>: {song.name}</div>
+                <div className="song-card-dynamic">
+                    <div className="p-3 text-center"><span className="d-inline-block song-card-dynamic__info px-3 py-1 border"><Artist id={song.artistId}/>: {song.name}</span></div>
                     <div className="px-3 pb-3">
-                        <audio controls autoPlay={true} className="w-100">
+                        <audio controls autoPlay={false} className="w-100">
                             <source src={song.url} type="audio/mpeg"/>
                             <p>
                                 Your browser doesn't support HTML5 audio.

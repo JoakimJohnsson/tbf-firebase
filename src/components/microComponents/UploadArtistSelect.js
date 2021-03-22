65d1c91ee008412e40107f87eb09f0ead5e32d9a
JoakimJohnsson <joakim.johnsson@su.se>
2021-03-22 05:52:25 +0100

    refactoring duplicated upload artist select

--- src/components/microComponents/UploadArtistSelect.js
+++ src/components/microComponents/UploadArtistSelect.js
@@ -9,13 +9,18 @@ const UploadArtistSelect = ({artistId, onChange}) => (
         onChange={onChange}>
         <option value="select">-- Select --</option>
         <option value="15th-22">15th 22</option>
+        <option value="the-alabama-alcoholics">The Alabama Alcoholics</option>
+        <option value="a-blueprint">A Blueprint</option>
         <option value="the-baseball-field">The Baseball field</option>
         <option value="the-bodonis">The Bodonis</option>
-        <option value="fredrik-svensson">Fredrik Svensson</option>
+        <option value="boys-on-heroin">Boys on Heroin</option>
+        <option value="fangorn">Fangorn</option>
+        <option value="h-2-0">H2O</option>
         <option value="mc-bomb">MC Bomb</option>
         <option value="music-ninja">Music / Ninja</option>
         <option value="olle">Olle</option>
         <option value="satans-galjonsfigurer">Satans Galjonsfigurer</option>
+        <option value="fredrik-svensson">Fredrik Svensson</option>
     </select>
 );
 

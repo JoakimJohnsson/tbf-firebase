216a1b2b975b4934e979c4b5cee4458e2bb75ef7
JoakimJohnsson <joakim.johnsson@su.se>
2021-03-28 09:25:03 +0200

    removing image directory

--- src/constants/cols.js
+++ src/constants/cols.js
@@ -1,4 +1,4 @@
 export const RECORDS_FULLWIDTH = `col-6 col-lg-4 col-xl-3 col-xxl-2 p-3`;
 export const RECORDS_MAIN = `col-6 col-md-6 col-xl-3 col-xxl-2 p-3`;
 export const ARTISTS_FULLWIDTH = `col-6 col-lg-4 col-xl-3 col-xxl-2 p-3 `;
-export const ARTISTS_MAIN = `col-6 col-md-6 col-xl-3 col-xxl-2 p-3 `;
+export const ARTISTS_MAIN = `col-6 col-md-6 col-xl-3 col-xxl-2 p-3 `

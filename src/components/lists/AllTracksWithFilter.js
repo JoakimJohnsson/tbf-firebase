73ac0e174ec8707d06164dd61fc12e8fb7c41132
JoakimJohnsson <joakim.johnsson@su.se>
2021-05-09 20:27:33 +0200

    refactoring

--- src/components/lists/AllTracksWithFilter.js
+++ src/components/lists/AllTracksWithFilter.js
@@ -3,42 +3,24 @@ import LoadingStandard from "../microComponents/Loading/LoadingStandard";
 import {FetchAllTracks} from "../../api-functions/api";
 import {TracksPlayerListItemWithImage} from "../apiComponents/TrackComponents";
 import {prepareFilterText} from "../microComponents/microComponentsHelper";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import FilterInput from "../formsAndInputs/FilterInput";
 
 const AllTracksWithFilter = () => {
     const [filter, setFilter] = useState('');
     const tracks = FetchAllTracks();
-    const clearInput = () => {
-        setFilter('')
-    }
 
     return tracks.length ?
         (
             <>
-                <div className="input-group col-12 col-sm-8 col-lg-6">
-                    <div className="input-group-prepend">
-                        <span className="input-group-text"><FontAwesomeIcon icon={"filter"}/></span>
-                    </div>
-                    <input id="filter"
-                           name="filter"
-                           type="text"
-                           className="form-control small"
-                           placeholder="Filter by artist or track"
-                           value={filter}
-                           onChange={event => setFilter(event.target.value)}
-                    />
-                    <div className="input-group-append">
-                        <button className="input-group-text" onClick={clearInput}><FontAwesomeIcon icon={"times"}/></button>
-                    </div>
-                </div>
+                <FilterInput filter={filter} setFilter={setFilter} placeHolder={"Filter by artist or track"}/>
                 <div className="row">
                     <div className="col-12 px-3 pt-3">
                         <ul className="tracks-player__list">
                             {tracks.filter(track => track.name.toLowerCase().includes(filter.toLowerCase()) ||
-                            track.artistId.toLowerCase().includes(prepareFilterText(filter)) || filter === '')
+                                track.artistId.toLowerCase().includes(prepareFilterText(filter)) || filter === '')
                                 .map(track =>
-                                <TracksPlayerListItemWithImage key={track.id} id={track.id}/>
-                            )}
+                                    <TracksPlayerListItemWithImage key={track.id} id={track.id}/>
+                                )}
                         </ul>
                     </div>
                 </div>

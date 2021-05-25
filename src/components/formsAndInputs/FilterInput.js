687c4f3566e8f9eb039f433eaff368c31d81c1a8
JoakimJohnsson <joakim.johnsson@su.se>
2021-05-25 16:09:33 +0200

    add a nice sticky top to filter inputs

--- src/components/formsAndInputs/FilterInput.js
+++ src/components/formsAndInputs/FilterInput.js
@@ -8,27 +8,30 @@ const FilterInput = ({filter, setFilter, placeHolder}) => {
     }
 
     return (
-        <div className="input-group col-12 col-sm-8 col-lg-6">
-            <div className="input-group-prepend">
-                <span className="input-group-text"><FontAwesomeIcon icon={"filter"}/></span>
-            </div>
-            <input id="filter"
-                   name="filter"
-                   type="text"
-                   className="form-control small"
-                   placeholder={placeHolder}
-                   value={filter}
-                   onChange={event => setFilter(event.target.value)}
-            />
-            {filter !== '' ?
-                <div className="input-group-append">
-                    <button className="input-group-text" onClick={clearInput}><FontAwesomeIcon icon={"times"}/></button>
+        <div className="sticky-top top-1">
+            <div className="input-group col-12 col-sm-8 col-lg-6">
+                <div className="input-group-prepend">
+                    <span className="input-group-text"><FontAwesomeIcon icon={"filter"}/></span>
                 </div>
-                :
-                ''
-            }
+                <input id="filter"
+                       name="filter"
+                       type="text"
+                       className="form-control small"
+                       placeholder={placeHolder}
+                       value={filter}
+                       onChange={event => setFilter(event.target.value)}
+                />
+                {filter !== '' ?
+                    <div className="input-group-append">
+                        <button className="input-group-text" onClick={clearInput}><FontAwesomeIcon icon={"times"}/></button>
+                    </div>
+                    :
+                    ''
+                }
+            </div>
         </div>
-    )};
+    )
+};
 
 
 export default FilterInput;
--- src/components/pages/ArtistsPage.js
+++ src/components/pages/ArtistsPage.js
@@ -5,7 +5,7 @@ import AllArtistsWithFilter from "../lists/AllArtistsWithFilter";
 const ArtistsPage = () => (
     <div className="row no-gutters">
         <div className="col-12">
-            <h1 className="mb-0 mb-sm-3">Artists</h1>
+            <h1 className="mb-3">Artists</h1>
         </div>
         <div className="col-12 mb-4">
             <AllArtistsWithFilter/>

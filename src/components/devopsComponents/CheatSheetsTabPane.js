79faad7c1919f67cbc77175b24c485a307a50ace
JoakimJohnsson <joakim.johnsson@su.se>
2021-09-28 07:40:19 +0200

    add tab pane partials

--- src/components/devopsComponents/CheatSheetsTabPane.js
+++ src/components/devopsComponents/CheatSheetsTabPane.js
@@ -1,27 +1,38 @@
 import React from 'react';
-import {Nav, Tab} from "react-bootstrap";
-import CheatSheetsGitTabPane from "./partialTabPanes/CheatSheetsGitTabPane";
+import {Tab} from "react-bootstrap";
+import IconLink from "../microComponents/Links/IconLink";
 
 const CheatSheetsTabPane = () => {
     return (
         <Tab.Pane eventKey="cheat-sheets-main">
             <div className="row article">
-                <div className="col-12 col-md-8">
+                <div className="col-12">
                     <h2>Cheat sheets</h2>
                     <p className="lead mb-5">
                         A cheat sheet is a concise set of notes used for quick reference. Cheat sheets were historically used by
                         students without an instructor or teacher's knowledge to cheat on a test or exam.
                     </p>
                 </div>
+                <div className="tab-pane-card">
+                    <h3>Git by Github</h3>
+                    <p>
+                        Git is the free and open source distributed version control system that's responsible for
+                        everything GitHub related that happens locally on your computer. This cheat sheet features the most
+                        important and commonly used Git commands for easy reference.
+                    </p>
+                    <IconLink link={"https://education.github.com/git-cheat-sheet-education.pdf"} text={"Git cheat sheet from Github"}
+                              icon={"external-link-alt"} external={true}/>
+                </div>
+                <div className="tab-pane-card">
+                    <h3>Node package manager</h3>
+                    <p>
+                        npm is the default package manager for the JavaScript runtime environment Node.js. It consists of a command line
+                        client, also called npm, and an online database of public and paid-for private packages, called the npm registry.
+                    </p>
+                    <IconLink link={"https://devhints.io/npm"} text={"Node package manager"}
+                              icon={"external-link-alt"} external={true}/>
+                </div>
             </div>
-            <Tab.Container id="tabs-git" defaultActiveKey="git" className="mb-3">
-                <Nav className="nav-tabs nav-tabs__secondary">
-                    <Nav.Link eventKey="git" className="nav-item nav-link">Git</Nav.Link>
-                </Nav>
-                <Tab.Content>
-                    <CheatSheetsGitTabPane/>
-                </Tab.Content>
-            </Tab.Container>
         </Tab.Pane>
     );
 }
--- src/components/devopsComponents/ResourcesTabPane.js
+++ src/components/devopsComponents/ResourcesTabPane.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Nav, Tab} from "react-bootstrap";
 import ResourcesIconsTabPane from "./partialTabPanes/ResourcesIconsTabPane";
+import ResourcesDatabasesTabPane from "./partialTabPanes/ResourcesDatabasesTabPane";
 
 const ResourcesTabPane = () => {
     return (
@@ -16,9 +17,11 @@ const ResourcesTabPane = () => {
             <Tab.Container id="resources-main-tabs" defaultActiveKey="icons" className="mb-3">
                 <Nav className="nav-tabs nav-tabs__secondary">
                     <Nav.Link eventKey="icons" className="nav-item nav-link">Icons</Nav.Link>
+                    <Nav.Link eventKey="databases" className="nav-item nav-link">Databases</Nav.Link>
                 </Nav>
                 <Tab.Content>
                     <ResourcesIconsTabPane/>
+                    <ResourcesDatabasesTabPane/>
                 </Tab.Content>
             </Tab.Container>
         </Tab.Pane>

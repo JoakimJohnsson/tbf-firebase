fcd6a77a2221030aa8d84063e732b6f9d973dab8
JoakimJohnsson <joakim.johnsson@su.se>
2021-12-28 08:51:50 +0100

    add more devops

--- src/components/devopsComponents/partialTabPanes/KarolinaBirthdayTabPane.js
+++ src/components/devopsComponents/partialTabPanes/KarolinaBirthdayTabPane.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Tab} from "react-bootstrap";
+import birthday2021 from "../../../images/birthday2021.jpg";
+import birthday2020 from "../../../images/im4.jpg";
 
 const KarolinaBirthdayTabPane = () => {
+    const [birthdayImage, setBirthdayImage] = useState(birthday2020);
+
+    const toggleBirthdayImage = () => {
+        if (birthdayImage === birthday2020) {
+            setBirthdayImage(birthday2021);
+        } else {
+            setBirthdayImage(birthday2020);
+        }
+    }
+
     return (
         <Tab.Pane eventKey="birthday">
             <div className="row article">
                 <div className="col-12 mb-2">
-                    <h2>Happy birthday!</h2>
+                    <h2 className="mb-5">Happy birthday!</h2>
+
+                    <div className="row justify-content-center">
+                        <div className="col-12 col-xl-8 d-flex justify-content-center align-items-center flex-column">
+
+                            <img src={birthdayImage} className="birthday-image mb-3"/>
+
+                            <button className="btn btn-secondary " onClick={toggleBirthdayImage}>Clickme</button>
+
+
+                        </div>
+                    </div>
+
 
                     {/*Insert happy birthday component here*/}
 
